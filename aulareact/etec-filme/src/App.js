fc8fe0c18328b00df069e8c45a6954ef75a4b235
Camilatrentino <camila.f.trentino@gmail.com>
2024-08-01 11:02:34 -0300

    dia 01/08

---
+++ aulareact/etec-filme/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <h1>ETEC MCM</h1>
+    <p>ola mundo</p>
+    </>
+  );
+}
+
+export default App;
